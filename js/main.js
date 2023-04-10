7fadf15f6a71e00017e197fa6358119174390d60
Julien <66158548+alucardness@users.noreply.github.com>
2023-04-10 23:55:50 +0300

    Start Set - ready till combat chapter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-function setYear() {
-  const yearEl = document.querySelector("footer .year");
-  const currentYear = new Date().getFullYear();
+const navigation = document.querySelector('#navbarToggleExternalContent');
+const navigationLinks = navigation.querySelectorAll('a');
+const navbar = document.querySelector('.navbar');
+const book = document.querySelector('.book-section');
 
-  yearEl.textContent = currentYear;
-}
+window.addEventListener('wheel', function (event) {
+    if (navbar.classList.contains('hidden') === false) return;
+
+    toggleNavbar();
+});
+
+navigationLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        collapseNavigation();
+    });
+});
+
+book.addEventListener('click', () => {
+    collapseNavigation();
+    toggleNavbar()
+});
 
 setYear();
+
+function collapseNavigation() {
+    if (navigation === null) return;
+
+    bootstrap.Collapse.getOrCreateInstance(navigation).toggle();
+    toggleNavbar();
+}
+
+function setYear() {
+    const yearEl = document.querySelector("footer .year");
+    const currentYear = new Date().getFullYear();
+
+    yearEl.textContent = currentYear;
+}
+
+function toggleNavbar() {
+    navbar.classList.toggle('hidden');
+}
